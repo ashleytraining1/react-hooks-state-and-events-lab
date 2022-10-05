76bae9587cee78ccd73b7712fb3388b60cf3df88
ashley <ashley.oketo@student.moringaschool.com>
2022-10-05 21:28:20 +0300

    Completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [whenClicked, setMode] = useState(false);
+
+  function handleClick(){
+    setMode((whenClicked) => !whenClicked)
+  }
+
+  const itemClass = whenClicked ? "in-cart" : "";
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>
+        { whenClicked ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+// import React,{useState} from "react";
+
+
+// function Item({ name, category }) {
+//   const [whenClicked, setMode] = useState(false);
+
+//   function handleClick(){
+//     setMode((whenClicked) => !whenClicked)
+//     const itemClass = whenClicked ? "in-cart" : "";
+//   }
+//   return (
+//     <li className={itemClass}>
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button className="add" onClick={handleClick}>{ whenClicked ? "Erase From Cart" : "Add to Cart"}</button>
+//     </li>
+//   );
+// }
+// //   return (
+// //     <li className="">
+// //       <span>{name}</span>
+// //       <span className="category">{category}</span>
+// //       <button className="add">Add to Cart</button>
+// //     </li>
+// //   );
+// // }
+
+// export default Item;
